4506ae0e96321decd6ccb55ef87b081be8d549b2
roxayp <roxanne.proulx87@gmail.com>
2023-02-22 15:41:55 -0500

    initial commit, some HTML

---
+++ script.js
@@ -0,0 +1,12 @@
+// create a 16x16 grid
+
+// create a function that changes the color of the grid when the mouse hovers over it
+
+// create a button that clears the grid and asks the user for a new grid size
+
+// create a button that changes the color of the grid to a random color
+
+// create a button that changes the color of the grid to a black and white gradient
+
+// create a button that changes the color of the grid to a rainbow gradient
+
